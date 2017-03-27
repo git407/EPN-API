5d6c9e0bc657ea24906a741c48c5bd2ab8531c50
tinyerror <sebaskub+git@gmail.com>
2017-03-27 20:49:45 +0200

    Added example with transactions request from date to date

---
+++ example.php
@@ -0,0 +1,12 @@
+﻿<?php
+require('clEPNCabinetAccess.php');
+$api = new clEpnCabinetAccess('apikey','privatekey');
+// add requests
+$api->AddRequestGetTransactions('transactions',0,'2017-03-22','2017-03-23','open','waiting','sub',1,200);
+// Execute queries
+$api->RunRequests();
+// Dump the results
+print_r($api->GetRequestResult('transactions'));
+print_r($api->LastError());
+
+?>
